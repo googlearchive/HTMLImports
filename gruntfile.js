c1f8db318d34c045982bf4fbed66e6dc8073f78d
Scott Miles <sjmiles@google.com>
2013-03-05 18:33:30 -0800

    factor ComponentDocument material out of CustomElements repository

---
+++ gruntfile.js
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2013 The Toolkitchen Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+module.exports = function(grunt) {
+  HTMLComponents = [
+    'src/HTMLComponents.js'
+  ];
+  grunt.initConfig({
+    uglify: {
+      HTMLComponents: {
+        /*
+        options: {
+          sourceMap: 'html-components.min.source-map.js'
+        },
+        */
+        files: {
+          'html-components.min.js': HTMLComponents
+        }
+      }
+    },
+    yuidoc: {
+      compile: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        options: {
+          exclude: 'third_party',
+          paths: '.',
+          outdir: 'docs',
+          linkNatives: 'true',
+          tabtospace: 2,
+          themedir: '../docs/doc_themes/simple'
+        }
+      }
+    },
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  // plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
+
+  // tasks
+  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('minify', ['uglify']);
+  grunt.registerTask('docs', ['yuidoc']);
+};
+
