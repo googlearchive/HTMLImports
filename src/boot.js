11c8836c685c30552a19aacc147a4daca1671ff5
Steve Orvell <steve@orvellia.com>
2013-10-07 14:37:15 -0700

    wip (commented) attempt to simulate imports loaded via script tag.

--- src/boot.js
+++ src/boot.js
@@ -40,4 +40,38 @@ if (document.readyState === 'complete' ||
   window.addEventListener('DOMContentLoaded', bootstrap);
 }
 
+/*
+var bootstrapper = bootstrap;
+
+if (HTMLImports.useNative) {
+  function findScriptPath(fileName) {
+    var script = document.querySelector('script[src*="' + fileName + '"]');
+    var src = script.attributes.src.value;
+    return src.slice(0, src.indexOf(fileName));
+  }
+
+  function nativeBootstrap() {
+    var s = document.createElement('script');
+    s.async = true;
+    s.src = findScriptPath('boot.js') + 'bootstrap-native.js';
+    document.head.appendChild(s);
+  }
+  bootstrapper = nativeBootstrap;
+}
+
+// Allow for asynchronous loading when minified
+// readyState 'interactive' is expected when loaded with 'async' or 'defer' attributes
+// note: use interactive state only when not on IE since it can become 
+// interactive early (see https://github.com/mobify/mobifyjs/issues/136)
+if (document.readyState === 'complete' ||
+    (document.readyState === 'interactive' && !window.attachEvent)) {
+  bootstrapper();
+} else {
+  window.addEventListener('DOMContentLoaded', bootstrapper);
+}
+
+
+// exports
+HTMLImports.bootstrap = bootstrap;
+*/
 })();
