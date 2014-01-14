fcc8abed8d13689d0b721804ad354f4dd2fc2454
Steve Orvell <steve@orvellia.com>
2014-01-14 08:54:38 -0800

    simplify boot, wip.

--- src/boot.js
+++ src/boot.js
@@ -19,33 +19,43 @@ if (typeof window.CustomEvent !== 'function') {
   };
 }
 
-function bootstrap() {
-  // TODO(sorvell): SD polyfill intrusion
-  var doc = window.ShadowDOMPolyfill ? 
-      window.ShadowDOMPolyfill.wrapIfNeeded(document) : document;
-  // preload document resource trees
-  HTMLImports.importer.load(doc, function() {
-    HTMLImports.parser.parse(doc, function() {;
-      HTMLImports.ready = true;
-      HTMLImports.readyTime = new Date().getTime();
-      // send HTMLImportsLoaded when finished
-      //console.warn('firing HTMLImportsLoaded');
-      doc.dispatchEvent(
-        new CustomEvent('HTMLImportsLoaded', {bubbles: true})
-      );
-    })
-  });
+// TODO(sorvell): SD polyfill intrusion
+var doc = window.ShadowDOMPolyfill ? 
+    window.ShadowDOMPolyfill.wrapIfNeeded(document) : document;
+
+function notifyReady() {
+  HTMLImports.ready = true;
+  HTMLImports.readyTime = new Date().getTime();
+  // send HTMLImportsLoaded when finished
+  //console.warn('firing HTMLImportsLoaded');
+  // TODO(sorvell): event is not useful if it fires too early.
+  doc.dispatchEvent(
+    new CustomEvent('HTMLImportsLoaded', {bubbles: true})
+  );
 }
 
-// Allow for asynchronous loading when minified
-// readyState 'interactive' is expected when loaded with 'async' or 'defer' attributes
-// note: use interactive state only when not on IE since it can become 
-// interactive early (see https://github.com/mobify/mobifyjs/issues/136)
-if (document.readyState === 'complete' ||
-    (document.readyState === 'interactive' && !window.attachEvent)) {
-  bootstrap();
+if (HTMLImports.useNative) {
+  notifyReady();
 } else {
-  window.addEventListener('DOMContentLoaded', bootstrap);
+  function bootstrap() {
+    // preload document resource trees
+    HTMLImports.importer.load(doc, function() {
+      HTMLImports.parser.parse(doc, function() {;
+        notifyReady();
+      })
+    });
+  }
+
+  // Allow for asynchronous loading when minified
+  // readyState 'interactive' is expected when loaded with 'async' or 'defer' attributes
+  // note: use interactive state only when not on IE since it can become 
+  // interactive early (see https://github.com/mobify/mobifyjs/issues/136)
+  if (document.readyState === 'complete' ||
+      (document.readyState === 'interactive' && !window.attachEvent)) {
+    bootstrap();
+  } else {
+    window.addEventListener('DOMContentLoaded', bootstrap);
+  }
 }
 
 })();
