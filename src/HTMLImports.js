cde4a8a94e6abda8efe7b026476298d32e459e6a
Daniel Freedman <dfreedm@google.com>
2014-02-03 17:16:26 -0800

    Fix SD Polyfill + document.baseURI
    I assumed (incorrectly) that SD Polyfill provided wrappers for document.baseURI

--- src/HTMLImports.js
+++ src/HTMLImports.js
@@ -171,14 +171,16 @@ Object.defineProperty(document, '_currentScript', currentScriptDescriptor);
 Object.defineProperty(mainDoc, '_currentScript', currentScriptDescriptor);
 
 // Polyfill document.baseURI for browsers without it.
-// ShadowDOM Polyfill wrapper should handle this automatically.
 if (!document.baseURI) {
-  Object.defineProperty(document, 'baseURI', {
+  var baseURIDescriptor = {
     get: function() {
       return window.location.href;
     },
     configurable: true
-  });
+  };
+
+  Object.defineProperty(document, 'baseURI', baseURIDescriptor);
+  Object.defineProperty(mainDoc, 'baseURI', baseURIDescriptor);
 }
 
 // TODO(sorvell): multiple calls will install multiple event listeners
